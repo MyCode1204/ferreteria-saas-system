088a6b67dad565a54666f256d69a27439e181ef1
Brayni Chavez <chavezcaruajulca.12@gmail.com>
2025-06-24 10:24:43 -0500

    Correccion en el Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -64,7 +64,7 @@ const Login = () => {
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
                 <h2 className="text-2xl font-bold text-center text-gray-800">
-                    Iniciar Sesión en <span className="text-indigo-600">{tenantId}</span>
+                    <span className="text-indigo-600">{tenantId}</span>
                 </h2>
                 <form onSubmit={handleSubmit} className="space-y-6">
                      <div>
